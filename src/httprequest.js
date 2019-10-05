a97a72256271be774527a43567b824fcd4e15ae7
Vectorial1024 <vincentwwy168@gmail.com>
2019-10-06 02:30:26 +0800

    Test update for the GitHub API

---
+++ src/httprequest.js
@@ -0,0 +1,31 @@
+/**
+ * Credits to https://gist.github.com/floriankraft/f84dbf3cf5c0ece164cfdb4246eab5d4
+ *
+ * Modified to make it error-free and usable.
+ *
+ * Utility function to utilize the Promise object to handle HTML requests.
+ * @param url
+ */
+function makeHttpRequest(url) {
+    return new Promise(function (resolve, reject) {
+        var request = new XMLHttpRequest();
+        request.onload = function () {
+            if (this.status === 200) {
+                resolve(this.response);
+            }
+            else {
+                reject(new Error(this.statusText));
+            }
+        };
+        request.onerror = function () {
+            var statusMessage = this.status.toString();
+            if (statusMessage.toString() == "") {
+                // Error that not even the server can respond: must be network-level errors
+                statusMessage = "Network Error";
+            }
+            reject(new Error("XMLHttpRequest complains: " + statusMessage));
+        };
+        request.open("GET", url);
+        request.send();
+    });
+}
